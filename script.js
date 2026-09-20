d2e0be3376b24ad6414421ab4fddc7081f330dce
Dragoş <edragos33@gmail.com>
2026-09-20 17:54:16 +0200

    added more variables

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let scorePlayerOne = document.querySelector("#player-one-score");
 let scorePlayerTwo = document.querySelector("#player-two-score");
 
+const playerOne = document.querySelector("#player-one");
+const playerTwo = document.querySelector("#player-two");
+const ball = document.querySelector("#ball");
+
 scorePlayerOne = 0;
 scorePlayerTwo = 0;
 
 
+console.log(playerOne);
+console.log(playerTwo);
+console.log(ball);
