e991f88063beae21418171c9775933a93a7cd441
Dragoş <edragos33@gmail.com>
2026-09-24 15:16:23 +0200

    score display added

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+let scores = document.querySelector(".player-score");
+
 let scorePlayerOne = document.querySelector("#player-one-score");
 let scorePlayerTwo = document.querySelector("#player-two-score");
+
 
 let speed = 10;
 
 let yPlayerOne = 0;
 let yPlayerTwo = 0;
 
+scorePlayerOne.textContent = yPlayerOne;
+scorePlayerTwo.textContent = yPlayerTwo;
+
+
 const playerOne = document.querySelector("#player-one");
 const playerTwo = document.querySelector("#player-two");
 const ball = document.querySelector("#ball");
+const gameContainer = document.querySelector(".game-conatiner");
 const keys = {};
+
+gameContainer.height = innerHeight;
 
 scorePlayerOne = 0;
 scorePlayerTwo = 0;
+
 
 document.addEventListener("keydown", e => {
     keys[e.key] = true;
@@ -27,6 +38,7 @@ function update() {
     // Player one
     if (keys["s"]) yPlayerOne += speed;
     if (keys["z"]) yPlayerOne -= speed;
+
 
     // Player Two
     if (keys["ArrowDown"]) yPlayerTwo += speed;
